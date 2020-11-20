b8c11b51ec187baf889bf79e3a29766ea350f890
lindsey <lindseyraven6@gmail.com>
2020-11-20 12:00:38 +0000

    app
    removing ref to config file

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template, request, redirect, url_for
 import session_items as session
 
 app = Flask(__name__)
-app.config.from_object('flask_config.Config')
 
 @app.route('/')
 def index():
